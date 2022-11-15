62745efc68eaf01dc112dfc5f57585d2584c1856
malunatera <marialauran1701@gmail.com>
2022-11-14 23:26:03 -0300

    actualizacion

--- index.js
+++ index.js
@@ -1,81 +1,123 @@
 
-
-// Funciones
-
 //Descuentos 
-function descuento1(p1, precioNoche) {
-    let resultado = 80 * p1 * precioNoche / 100
+const descuento1 = (p1, precioNoche) => {
+    let resultado = p1 * precioNoche / 1.2
     console.log(resultado)
     return resultado
 }
 
-function descuento2(p1, precioNoche) {
-    let resultado = 70 * p1 * precioNoche / 100
+const descuento2 = (p1, precioNoche) => {
+    let resultado = p1 * precioNoche / 1.3
     console.log(resultado)
     return resultado
 }
 
-function descuento3(p1, precioNoche) {
-    let resultado = 60 * p1 * precioNoche / 100
+const descuento3 = (p1, precioNoche) => {
+    let resultado = p1 * precioNoche / 1.4
     console.log(resultado)
     return resultado
 }
 
 
-//Calculo de descuento segun noches
-const descuentoxNoches = (x) => {
-    const precioNoche = 100
-    let resultado
-    if (x >= 1 && x <= 2) {
-        resultado = descuento1(x, precioNoche)
+// Edificios
+
+class Edificio {
+    constructor (id, nombre, ubicacion, precio){
+        this.id = id
+        this.nombre = nombre
+        this.ubicacion = ubicacion
+        this.precio = precio
     }
-    else if (x >= 3 && x <= 4) {
-        resultado = descuento2(x, precioNoche)
+    descuento (x) {
+        const precio = this.precio
+        let resultado
+        if (x >= 1 && x <= 2) {
+            resultado = descuento1(x, precio)
+        }
+        else if (x >= 3 && x <= 4) {
+            resultado = descuento2(x, precio)
+        }
+        else if (x >= 5) {
+            resultado = descuento3(x, precio)
+        }
+        console.log(resultado)
+        return resultado
     }
-    else if (x >= 5) {
-        resultado = descuento3(x, precioNoche)
-    }
-    return resultado
 }
+
+const palermo = new Edificio (1, `Palermo Apartments`, `Thames 2313`, 250)
+const guido = new Edificio (2, `Ayres de Recoleta Plaza`, `Guido 1980`, 200)
+const uriburu = new Edificio (3, `Ayres de Recoleta Uriburu`, `Uriburu 1756`, 180)
+
+
+const edificios = [palermo, guido, uriburu]
+console.log (edificios)
+
 
 
 
 // Variables
 
 let reserva
+let edificio
 let noches
+let precioxCantidad
 let seguirCotizando = true
+let edificioEscogido = true
 let decision
+
 
 //Cotizador 
 
 alert(`¡Bienvenidos a Ayres Apartments!`)
 reserva = prompt(`¿Desea realizar una cotización? Ingrese si o no`)
+
+
 while (seguirCotizando = true) {
     if (reserva.toLowerCase() === `si`) {
         noches = parseInt(prompt(`Ingrese la cantidad de noches que desea quedarse en numeros`))
-        const precioxCantidad = descuentoxNoches(noches)
-        alert(`El precio de tu reserva seria de ${precioxCantidad}USD, por las ${noches} noches.`)
-        console.log(precioxCantidad)
-        decision = prompt(`¿Desea seguir cotizando? Ingrese si o no`)
-        while (seguirCotizando = true){
-            if (decision.toLowerCase() === `si`){
-                noches = parseInt(prompt(`Ingrese la cantidad de noches que desea quedarse en numeros`))
-                const precioxCantidad = descuentoxNoches(noches)
-                alert(`El precio de tu reserva seria de ${precioxCantidad}USD, por las ${noches} noches.`)
-                console.log(precioxCantidad)
-                decision = prompt(`¿Desea seguir cotizando? Ingrese si o no`)        
-            }
-            else if (decision.toLowerCase()===`no`){
-                seguirCotizando = false
-                break
+        while (seguirCotizando = true) {
+            if(noches){
+                edificio = parseInt(prompt(`¿En que edificio desea hospedarse? Ingrese opcion segun el numero.  Opciones: 1. Palermo 2. Guido 3. Uriburu`))
+                while (seguirCotizando = true) {
+                    if (edificio >= 1 && edificio <=3) {
+                    const edificioCliente = edificios.find(lugar => lugar.id === edificio)
+                    console.log(edificioCliente)
+                    precioxCantidad = edificioCliente.descuento(noches)
+                    console.log(precioxCantidad)            
+                    alert(`El precio de tu reserva seria de ${Math.round(precioxCantidad)}USD, por ${noches} noches en ${edificioCliente.nombre}.`)    
+                    break
+                    }
+                    else{
+                    alert (`No escogio un edificio dentro de las opciones`)
+                    edificio = parseInt(prompt(`¿En que edificio desea hospedarse? Ingrese opcion segun el numero.  Opciones: 1. Palermo 2. Guido 3. Uriburu`))
+                    continue
+                    }
+                }
             }
             else{
-                alert('Disculpe, no ingreso una respuesta valida')
-                decision = prompt(`¿Desea seguir cotizando? Ingrese si o no`)
+                alert(`Disculpe, no ingreso una respuesta valida`)
+                noches = parseInt(prompt(`Ingrese la cantidad de noches que desea quedarse en numeros`))   
+                continue         
             }
+            seguirCotizando = false
+            break    
+        }
+        decision = prompt(`¿Desea seguir cotizando? Ingrese si o no`)    
+        if (decision.toLowerCase() === `si`){
+            continue
+        }
+        else if (decision.toLowerCase()===`no`){
+            seguirCotizando = false
+            break
+        }
+        else{
+            alert('Disculpe, no ingreso una respuesta valida')
+            decision = prompt(`¿Desea seguir cotizando? Ingrese si o no`)
+            continue
         }
     }
+
     else if (reserva.toLowerCase()===`no`){
         seguirCotizando = false
         break
@@ -83,9 +125,10 @@ while (seguirCotizando = true) {
     else {
         alert(`Disculpe, no ingreso una respuesta valida`)
         reserva = prompt(`¿Desea realizar una cotización? Ingrese si o no`)
+        continue
     }     
-    seguirCotizando = false
-    break
+    // seguirCotizando = false
+    // break
 }
     
 alert(`Bienvenido a nuestra web, podes ver nuestros servicios para una proxima reserva`)
